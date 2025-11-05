35ab281b44718c2ac79379fbd2e01cf348f71e03
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-11-04 22:42:29 -0600

    Header + About done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,14 +3,18 @@ import image from "../Assets/Me.jpeg";
 
 function About() {
     return (
-        <div class="m-5 px-5 text-center border-bottom"> 
-            <div class="pb-5 d-flex justify-content-center">
-                <img src={image} alt="Photo of Abdullah Ali" class="custom-circle shadow border border-dark" />
+        <div className="m-5 px-5 text-center"> 
+            <div className="pb-5 d-flex justify-content-center">
+                <img src={image} alt="Photo of Abdullah Ali" className="custom-circle shadow border border-dark border-5" />
             </div>
-            <p class="mx-5 px-5 display-4 fs-1 fw-bold " style={{ color: "#00006aff" }}>
-                Hi, I'm Abdullah Ali. 
-                Here you can check out my experiences, projects, and skills!
-            </p> 
+            <div className="mx-5 pt-4 px-5">
+                <p className="py-1 mx-5 px-5 display-4 fs-1 fw-bolder" style={{ color: "#00006aff" }}>
+                    Hi, I'm <span style={{ color: "#005d17ff" }}>Abdullah Ali</span> 
+                </p>
+                <p className="py-1 mx-5 px-5 display-4 fs-2 fw-bold" style={{ color: "#00006aff" }}>
+                    I turn concepts into code, code into experiences
+                </p>
+            </div>
         </div>
     )
 }
