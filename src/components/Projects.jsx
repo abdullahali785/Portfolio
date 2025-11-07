2868126da76175b5db02e564bb3aae1c3e64be83
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-11-06 23:43:01 -0600

    Projects 6/6

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -34,24 +34,24 @@ function Projects() {
             <div className="col"> 
                 <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg zoom" style={{backgroundImage: `url(${image})`}}>
                     <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1"> 
-                        <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold text-dark">AquaVitals</h3>
-                        <p className="text-dark mt-auto">A 3 step ML model to estimate water transparency and fish survival rates using real-time rainfall and temperature data.</p>
+                        <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold text-dark">Image Text Analyzer</h3>
+                        <p className="text-dark mt-auto">A Python tool that extracts text from an image using EasyOCR and PyTesseract, and generates an AI-powered explanation of the text using OpenAI's GPT model.</p>
                     </div> 
                 </div>
             </div> 
             <div className="col"> 
                 <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg zoom" style={{backgroundImage: `url(${image})`}}>
                     <div className="d-flex flex-column h-100 p-5 pb-3 text-shadow-1"> 
-                        <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold text-dark">Abdullah's Books</h3> 
-                        <p className="text-dark mt-auto">A full-stack book tracker using Node.js (Express) and PostgreSQL with CRUD functionality and a responsive UI.</p>
+                        <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold text-dark">Travel Tracker</h3> 
+                        <p className="text-dark mt-auto">A web app that lets you track the travels of your entire family. Users can add family members, log visited countries, and view dynamic maps showing travel history.</p>
                     </div>
                 </div> 
             </div> 
             <div className="col">
                 <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg zoom" style={{backgroundImage: `url(${image})`}}>
                     <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1"> 
-                        <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold text-dark">HTTP Server</h3>
-                        <p className="text-dark mt-auto">A multithreaded HTTP server built with raw sockets, thread pools, and stream-based response.</p> 
+                        <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold text-dark">Fitness Made Feasible</h3>
+                        <p className="text-dark mt-auto">A web app that locates nearby gyms and, based on the muscle you want to train, provides exercises with instructions and visual GIF demonstrations.</p> 
                     </div> 
                 </div> 
             </div> 
