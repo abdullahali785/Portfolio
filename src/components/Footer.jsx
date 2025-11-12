229f071460739dce4d3916e68953a852cbeabda2
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-11-11 23:41:13 -0600

    projects UI done + skills start

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer>
         <div className="container"> 
             <div className="pt-3 my-4"> 
-                <p className="text-center fs-3 fw-bold" style={{ color: "#000000ff" }}>Made in React.js by Abdullah with 💙</p> 
+                <p className="text-center fs-3 fw-bold" style={{ color: "#000000ff" }}>Made by Abdullah in React.js with 💙</p> 
             </div> 
         </div>
         <div style={{ backgroundColor: "#00006aff"}}>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
+import pythonIcon from "../Assets/python-icon.gif";
+import javaIcon from "../Assets/java-icon.png";
 
 function Skills() {
-    
+    return (
+    <div className="container px-4 py-5" id="custom-cards"> 
+        <h2 className="pb-2 fs-1 fw-bold border-bottom text-center">My Skills</h2>
+        <img src={pythonIcon} alt="Python icon"/>
+        <img src={javaIcon} alt="Java icon"/>
+    </div>
+    );
 }
 
 export default Skills;
