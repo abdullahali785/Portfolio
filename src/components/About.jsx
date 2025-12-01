01163ed6a1f17fe616efe2d1a258bc5ecf03a5fd
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-11-30 23:05:38 -0600

    image resize + dynamic functionality

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
     return (
     <div id="about" className="mt-5 mb-2 mx-5 pt-5 px-5 text-center"> 
         <div className="pb-5 d-flex justify-content-center">
-            <img src={image} alt="Photo of Abdullah Ali" className="custom-circle shadow border border-dark border-5 zoom"/>
+            <img src={image} alt="Photo of Abdullah Ali" className="rounded-circle img-fluid shadow border border-dark border-5 zoom"/>
         </div>
         <div className="px-3 py-4 text-center">
             <p className="display-4 fs-1 fw-bolder" style={{ color: "#000000ff" }}>
