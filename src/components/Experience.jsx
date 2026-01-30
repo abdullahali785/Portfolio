bd9e11acfe97d4bed70b398e7a633561840a3044
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-01-30 15:40:13 -0600

    ordering of skill icons

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,9 +8,9 @@ function Experience() {
                 <h1 className="pb-3 fs-1 fw-bold bg-dark text-white">My Experience</h1> 
                 <p className="col-lg-8 mx-auto fs-3 fw-semibold lead bg-dark text-white">Technology Help Desk Technician</p> 
                 <p className="col-lg-8 mx-auto lead bg-dark text-white">
-                    I diagnosed and resolved <span className="fw-bold bg-dark">hardware, software, and network</span> issues for students, faculty, and staff.
-                    Documented <span className="fw-bold bg-dark">350+</span> tickets demonstrating strong problem-solving and clear communication skills.
-                    Made <span className="fw-bold bg-dark">1000+</span> edits to the Technology Help Desk Wiki, a documentation of more than <span className="fw-bold bg-dark">850</span> pages.
+                    I diagnosed and resolved <span className="fw-bold bg-dark">hardware, software, and network</span> issues for students, faculty, and staff.<br></br>
+                    Documented <span className="fw-bold bg-dark">350+</span> tickets demonstrating strong problem-solving and clear communication skills.<br></br>
+                    Made <span className="fw-bold bg-dark">1000+</span> edits to the Technology Help Desk Wiki, a documentation of more than <span className="fw-bold bg-dark">850</span> pages.<br></br>
                     Collaborated with other Luther departments and ITS sub-teams to ensure smooth operations.
                 </p> 
             </div> 
